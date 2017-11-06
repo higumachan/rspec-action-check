a0ec413ad8747cc5afab771e5018902d1d4aae00
Yuta Hinokuma <yuta@abeja.asia>
2017-11-06 13:50:36 +0000

    add action after branch spec

--- spec/rspec/example/action_after_branch_spec.rb
+++ spec/rspec/example/action_after_branch_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Action after branch' do
   end
 
   actions 'some actions like a scenario test story' do
-    branch 'branc1' do
+    branch 'branch1' do
       action 'set some_state1 = 100' do
         @some_state1 = 100
       end
@@ -16,7 +16,7 @@ RSpec.describe 'Action after branch' do
       end
     end
 
-    branch 'branc2' do
+    branch 'branch2' do
       action 'set some_state1 = 200' do
         @some_state1 = 200
       end
@@ -24,6 +24,11 @@ RSpec.describe 'Action after branch' do
       action 'set some_state2 = 100' do
         @some_state2 = 100
       end
+    end
+
+    check 'both state is not nil' do
+      expect(@some_state1).not_to be_nil
+      expect(@some_state2).not_to be_nil
     end
 
     action 'set some_state3 = 300' do
@@ -36,7 +41,7 @@ RSpec.describe 'Action after branch' do
     end
 
     check 'some_state3 = 300' do
-      expect(@some_state4).to eq 300
+      expect(@some_state3).to eq 300
     end
   end
 end
